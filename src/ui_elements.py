f30a8f2dc2020d6d1b783d65774e3e8217a9b253
marekmchl <marekmchl@proton.me>
2025-04-15 18:50:45 +0200

    Add padding to UI elements

--- src/ui_elements.py
+++ src/ui_elements.py
@@ -15,9 +15,9 @@ class Line(Frame):
         super().__init__(master)
         self.configure(bg = settings["colors"]["section_bg"])
         self.name_label = Label(self, text = name, justify = LEFT,bg = settings["colors"]["section_bg"], fg = settings["colors"]["line_fg"])
-        self.name_label.pack(side = LEFT, fill = BOTH, expand = True)
+        self.name_label.pack(side = LEFT, fill = BOTH, expand = True, padx = settings["sizes"]["line_padding"], pady = settings["sizes"]["line_padding"])
         self.value_label = Label(self, textvariable = value_var, justify = RIGHT,bg = settings["colors"]["line_bg"], fg = settings["colors"]["line_fg"])
-        self.value_label.pack(side = RIGHT, fill = BOTH, expand = True)
+        self.value_label.pack(side = RIGHT, fill = BOTH, expand = True, padx = settings["sizes"]["line_padding"], pady = settings["sizes"]["line_padding"])
 
 class Attribute(Frame):
     def __init__(self, master, name, value_var, inc_func, dec_func):
@@ -26,16 +26,16 @@ class Attribute(Frame):
 
         self.name_label = Label(self, text = name, justify = LEFT,bg = settings["colors"]["section_bg"], fg = settings["colors"]["line_fg"])
         # self.name_label.pack(side = LEFT, fill = BOTH, expand = True)
-        self.name_label.grid(row = 0, column = 0)
+        self.name_label.grid(row = 0, column = 0, padx = settings["sizes"]["line_padding"], pady = settings["sizes"]["line_padding"])
 
         self.value_label = Label(self, textvariable = value_var, justify = RIGHT,bg = settings["colors"]["line_bg"], fg = settings["colors"]["line_fg"])
         # self.value_label.pack(side = RIGHT, fill = BOTH)
-        self.value_label.grid(row = 0, column = 1)
+        self.value_label.grid(row = 0, column = 1, padx = settings["sizes"]["line_padding"], pady = settings["sizes"]["line_padding"])
 
         self.add_button = Button(self, text = "+", bg = settings["colors"]["line_bg"], activebackground = settings["colors"]["line_fg"], fg = settings["colors"]["line_fg"], activeforeground = settings["colors"]["line_bg"], command = inc_func)
         # self.add_button.pack(side = RIGHT, fill = BOTH)
-        self.add_button.grid(row = 0, column = 2)
+        self.add_button.grid(row = 0, column = 2, padx = settings["sizes"]["line_padding"], pady = settings["sizes"]["line_padding"])
 
         self.sub_button = Button(self, text = "-", bg = settings["colors"]["line_bg"], activebackground = settings["colors"]["line_fg"], fg = settings["colors"]["line_fg"], activeforeground = settings["colors"]["line_bg"], command = dec_func)
         # self.sub_button.pack(side = RIGHT, fill = BOTH)
-        self.sub_button.grid(row = 0, column = 3)
+        self.sub_button.grid(row = 0, column = 3, padx = settings["sizes"]["line_padding"], pady = settings["sizes"]["line_padding"])
