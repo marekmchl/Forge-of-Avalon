4cc0a81d749fce07cf0370c6ddf7f4db41fec59e
marekmchl <marekmchl@proton.me>
2025-04-15 18:12:24 +0200

    Fix function that loads settings, modify default settings

--- src/settings.py
+++ src/settings.py
@@ -3,14 +3,20 @@ from json import dumps, loads
 def load_settings():
     try:
         file = open("current_settings.json", "r")
+        settings_dict = loads(file.read())
+        file.close()
+        return settings_dict
     except Exception:
         file = open("default_settings.json", "r")
-        new_file = open("current_settings.json", "w")
+        new_file = open("current_settings.json", "x")
         new_file.write(file.read())
         new_file.close()
-    settings_dict = loads(file.read())
-    file.close()
-    return settings_dict
+        file.close()
+        file = open("current_settings.json", "r")
+        settings_dict = loads(file.read())
+        file.close()
+        return settings_dict
+
 
 def save_settings(settings_dict):
     file = open("current_settings.json", "w")
