246cdfcb1437d6df08d1fa3302640eb7f4582eb3
marekmchl <marekmchl@proton.me>
2025-04-15 18:09:49 +0200

    Fix how state works, add state variables, add function to correctly calculate minimal level

--- src/state.py
+++ src/state.py
@@ -9,21 +9,29 @@ def make_updatable(master, state_dict):
             state_dict[name] = IntVar(master, value, name)
         elif isinstance(value, float):
             state_dict[name] = DoubleVar(master, value, name)
-
     return state_dict
 
-def load_state(master):
+def load_state():
     try:
         file = open("current_state.json", "r")
+        state_dict = loads(file.read())
+        file.close()
+        return state_dict
     except Exception:
         file = open("default_state.json", "r")
-        new_file = open("current_state.json", "w")
+        new_file = open("current_state.json", "x")
         new_file.write(file.read())
         new_file.close()
-    state_dict = loads(file.read())
-    file.close()
-    state_dict = make_updatable(master, state_dict)
-    return state_dict
+        file.close()
+        file = open("current_state.json", "r")
+        state_dict = loads(file.read())
+        file.close()
+        return state_dict
+
+state_dict = load_state()
+
+def update_level():
+    state_dict["Level"].set(1 + max(state_dict["Invested_attribute_points"].get(), state_dict["Invested_skill_points"].get()))
 
 def make_savable(dict):
     pass
